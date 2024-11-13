9a4976740fb6d9b184463f05768db42546c03a29
rguixaro <rguixaro@protonmail.ch>
2024-11-13 19:21:01 +0100

    feat(logic): add link-hashing algorithm

--- server/app/link.py
+++ server/app/link.py
@@ -1,6 +1,32 @@
-import random
-import string
+
+from dotenv import load_dotenv
+from  hashlib import md5
+import os
+
+from .models import Link
+
+load_dotenv()
+SHORT_URL_CHAR_SIZE = int(os.getenv("SHORT_URL_CHAR_SIZE"))
 
 
-def generate_link():
-    return ''.join(random.choices(string.ascii_letters + string.digits, k=7))
+def generate_link(longURL):
+    hash = encode_to_md5(longURL)
+    if hash == None:
+        return None
+    numberOfCharsInHash = len(hash)
+    counter = 0
+    while counter < numberOfCharsInHash - SHORT_URL_CHAR_SIZE:
+        slug = hash[counter:counter + SHORT_URL_CHAR_SIZE]
+        link = Link.objects.filter(slug = slug)
+        if not link:
+            return slug
+        counter += 1
+
+def encode_to_md5(longURL):
+    try:
+        encoder = md5()
+        encoder.update(longURL.encode('utf-8'))
+        hash = encoder.hexdigest()
+        return hash
+    except:
+        return None
